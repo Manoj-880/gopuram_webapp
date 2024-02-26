7e896b776148f8116aa0d1e43042925816491534
Manoj-880 <manoj.inamanamelluri123@gmail.com>
2024-02-26 10:28:35 +0530

    removed console.logs in image adding to local storage

--- src/components/gallery/addImage.jsx
+++ src/components/gallery/addImage.jsx
@@ -6,15 +6,7 @@ const AddImage = ({ setShowModal }) => {
     const handleImageChange = (event) => {
         const file = event.target.files[0];
         setSelectedImage(file);
-    
-        // Show image preview
         const reader = new FileReader();
-        reader.onload = () => {
-            const imagePreviewUrl = reader.result;
-            // Set the image preview URL to a state variable or directly display it in the UI
-            // For now, let's just log it
-            console.log("Image preview URL:", imagePreviewUrl);
-        };
         reader.readAsDataURL(file);
     };
     
@@ -30,7 +22,6 @@ const AddImage = ({ setShowModal }) => {
             const updatedImages = storedImages ? JSON.parse(storedImages) : [];
             updatedImages.push(imageBase64);
             localStorage.setItem('galleryImages', JSON.stringify(updatedImages));
-            console.log("Image uploaded:", imageBase64);
             setShowModal(false); 
         };
         reader.readAsDataURL(selectedImage);
